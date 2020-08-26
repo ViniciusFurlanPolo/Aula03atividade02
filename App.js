2b13703f737883f1a918ee865c655b47bc95c9ca
ViniciusFurlanPolo <vinimurta4740@gmail.com>
2020-08-25 22:48:57 -0300

    Atividade02

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
+import Estilo from './src/Estilo';
 
 export default function App() {
+  const [num1, setnum1] = useState(0)
+  const [num2, setnum2] = useState(0)
+  const [res, setRes] = useState('')
+
+  const soma = () => parseInt(setRes(num1 + num2)) 
+  const sub = () => parseInt(setRes(num1 - num2)) 
+  const mult = () => parseInt(setRes(num1 * num2)) 
+  // const div = () => parseInt(setRes(num1 / num2))
+  function div () {
+    if (num2 != 0){
+      setRes(num1 / num2)
+      
+    }else{
+      <Alert severity="error">Não existe divisão por 0</Alert>
+    }
+  } 
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+      <View style={styles.container}>
+        <TextInput keyboardType= 'phone-pad'
+        placeholder = "Digite um Numero Inteiro"
+        style={Estilo.numpad1}
+        onChangeText = {num1 => setnum1(parseInt(num1))}
+        />
+        <TextInput keyboardType= 'phone-pad'
+        placeholder = "Digite um Numero Inteiro"
+        style={Estilo.numpad1}
+        onChangeText = {num2 => setnum2(parseInt(num2))}
+        />
+        <StatusBar style="auto" />
+        <Text style={Estilo.numpad1}> {res} </Text>
     </View>
+    <View style={Estilo.botoes}>
+      <Button title = "Soma" 
+          onPress =  {soma} ></Button>
+      <Button title = "Subtração" 
+          onPress =  {sub} ></Button>
+      <Button title = "Multiplicação" 
+          onPress =  {mult} ></Button>
+      <Button title = "Divisaõ" 
+          onPress =  {div} ></Button>
+    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
   },
+  
 });
---
+++ src/Estilo.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+    numpad1: {
+        backgroundColor: 'white',
+          
+    },
+    botoes: {
+        flexGrow: 1,
+        flexDirection: 'row',
+        alignContent:'flex-start',
+        backgroundColor: 'black',
+    }
+
+
+})
